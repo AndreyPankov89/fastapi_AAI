5eb21a9b913649dd8ed04b35284edf606a48d81b
Your Name <apan89@mail.ru>
2025-05-07 23:13:01 +0500

    fix fastapi settings

--- backend/main.py
+++ backend/main.py
@@ -202,5 +202,5 @@ app.add_middleware(
 # Запускаем сервер
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, root_path="/api", reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000, root_path="/api")
     
